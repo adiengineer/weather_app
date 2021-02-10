b5fd8c94c1a0e8c2214f55d0ae97a8109e3f3733
adiengineer <adivj123@gmail.com>
2021-02-09 19:40:10 -0800

    first steps of weather app dev

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Adi Suhaana Saama Weather app</h1>
       </header>
+      <main>
+        {/* the main forecast component will go here */} 
+      </main>
+      <footer>
+        Created by Adi J for Saama New grad SWE assesment
+      </footer>
     </div>
   );
 }
